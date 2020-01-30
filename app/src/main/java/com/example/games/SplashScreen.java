65761038b746045cd10aab41b5cc4adf2133d1b6
KamikazeNB <kamikazenb@gmail.com>
2020-01-30 18:52:53 +0100

    added SplashScreen

---
+++ app/src/main/java/com/example/games/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.example.games;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Intent intent = new Intent(this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+    }
+}
